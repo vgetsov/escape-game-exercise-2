94c17ae220ca347243a120fc0f213682454c9df5
Victor Getsov <viktorgetsov@gmail.com>
2021-05-29 03:26:43 +0300

    clean up initial setup, install prettier and linters, add config files

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['react-app', 'plugin:prettier/recommended'],
+    rules: {
+        'no-console': 'warn',
+        'no-unused-vars': 'error',
+        'no-duplicate-imports': 'error',
+        'no-param-reassign': 'error',
+        'no-shadow': 'error',
+        'prefer-const': 'error',
+        'prefer-template': 'error',
+        'object-shorthand': 'error',
+    },
+}
