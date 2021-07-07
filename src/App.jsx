910fd6c19406843153a7a222dab069c6e67a6b98
Victor Getsov <viktorgetsov@gmail.com>
2021-07-07 23:34:00 +0300

    added router and helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,205 @@
-import React from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import { BrowserRouter as Router, Switch, Route, Link, NavLink, useParams, useRouteMatch } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
-const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = React.useState(localStorage.getItem(key) || initialState)
+export default function App() {
+    return (
+        <Router>
+            <div className="mx-auto mt-3 flex-content" style={{ width: 500 }}>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/" activeClassName="selected">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <NavLink to="/first-level">First level</NavLink>
+                        </li>
+                        <li>
+                            <Link to="/button">Button</Link>
+                        </li>
+                        <li>
+                            <Link to="/form">Form</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
 
-    React.useEffect(() => {
-        localStorage.setItem(key, value)
-    }, [value, key])
-
-    return [value, setValue]
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/first-level">
+                    <FirstLevel />
+                </Route>
+                <Route path="/button">
+                    <ButtonClick />
+                </Route>
+                <Route path="/form">
+                    <FormExample />
+                </Route>
+                <Route path="/danger">
+                    <Danger />
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
-const App = () => {
-    const stories = [
-        {
-            title: 'React',
-            url: 'https://reactjs.org/',
-            author: 'Jordan Walke',
-            num_comments: 3,
-            points: 4,
-            objectID: 0,
-        },
-        {
-            title: 'Redux',
-            url: 'https://redux.js.org/',
-            author: 'Dan Abramov',
-            num_comments: 2,
-            points: 5,
-            objectID: 1,
-        },
-    ]
+const Home = () => {
+    return (
+        <HelmetProvider>
+            <div className="mx-auto mt-3 flex-content" style={{ width: 500 }}>
+                <Helmet>
+                    <title>Home page</title>
+                </Helmet>
+                <h1>My Hacker Stories</h1>
+                <p>Welcome to our page. Take a look around, check out our content</p>
+                <Button variant="outline-primary w-50" className="m-1" href="/button">
+                    Check our buttons{' '}
+                </Button>{' '}
+                <Button variant="outline-success w-50" className="m-1" href="/form">
+                    Check our forms{' '}
+                </Button>{' '}
+            </div>
+        </HelmetProvider>
+    )
+}
 
-    const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
+const FirstLevel = () => {
+    const { path, url } = useRouteMatch()
+    return (
+        <HelmetProvider>
+            <div className="mx-auto mt-3 flex-content" style={{ width: 500 }}>
+                <Helmet>
+                    <title>Choose a stage</title>
+                </Helmet>
+                <h2>Stages</h2>
+                <ul>
+                    <li>
+                        <Link to={`${url}/first-stage`}>First stage</Link>
+                    </li>
+                    <li>
+                        <Link to={`${url}/second-stage`}>Second stage</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path={path}>
+                        <h3>Please select a stage.</h3>
+                    </Route>
+                    <Route path={`${path}/:stageId`}>
+                        <Stage />
+                    </Route>
+                </Switch>
+            </div>
+        </HelmetProvider>
+    )
+}
 
-    const handleSearch = (event) => setSearchTerm(event.target.value)
-
-    const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+const Stage = () => {
+    const { stageId } = useParams()
 
     return (
-        <div className="mx-auto mt-3" style={{ width: 500 }}>
-            <h1>My Hacker Stories</h1>
-            <InputWithLabel id="search" value={searchTerm} type="text" onInputChange={handleSearch}>
-                <Text />
-                <strong>Search:</strong>
-            </InputWithLabel>
-            <p>
-                We are searching for <strong>{searchTerm}</strong>
-            </p>
-            <Button variant="outline-primary" className="m-1" href="https://react-bootstrap.github.io/">
-                Click me
-            </Button>{' '}
-            <hr />
-            <List list={searchedStories} />
-            <div className="shadow-lg p-3 mb-3 bg-body rounded">Larger shadow</div>
-            <div className="shadow p-3 mb-3 bg-body rounded">Regular shadow</div>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+        <div>
+            <h3>{stageId}</h3>
         </div>
     )
 }
 
-const Text = () => <p>Search through our database</p>
+const ButtonClick = () => {
+    return (
+        <HelmetProvider>
+            <div className="mx-auto mt-3 flex-content" style={{ width: 500 }}>
+                <Helmet>
+                    <title>Button variants</title>
+                </Helmet>
+                <Button variant="outline-success mt-2 w-50" href="/">
+                    Success
+                </Button>{' '}
+                <Button variant="outline-danger mt-2 w-50" href="/danger">
+                    Danger
+                </Button>{' '}
+                <Button variant="outline-primary mt-2 w-50" href="/">
+                    Info
+                </Button>{' '}
+            </div>
+        </HelmetProvider>
+    )
+}
 
-const InputWithLabel = ({ id, value, type, onInputChange, children }) => (
-    <>
-        <label htmlFor={id}>{children}</label>
-        &nbsp;
-        <input id={id} type={type} value={value} onChange={onInputChange} />
-    </>
-)
+const FormExample = () => {
+    return (
+        <HelmetProvider>
+            <div className="mx-auto mt-3" style={{ width: 500 }}>
+                <Helmet>
+                    <title>Form variants</title>
+                </Helmet>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </HelmetProvider>
+    )
+}
 
-const List = ({ list }) => list.map((item) => <Item key={item.objectID} item={item} />)
+const Danger = () => {
+    return (
+        <HelmetProvider>
+            <div className="mx-auto mt-3 flex-content" style={{ width: 500 }}>
+                <Helmet>
+                    <title>Danger page</title>
+                </Helmet>
+                <p>Why did you click it?!</p>
+                <NavLink to="/">Try again, genius</NavLink>
+            </div>
+        </HelmetProvider>
+    )
+}
 
-const Item = ({ item }) => (
-    <div>
-        <span>
-            <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-    </div>
-)
+// const App = () => {
+//     return (
+//         <div className="mx-auto mt-3" style={{ width: 500 }}>
+//             <h1>My Hacker Stories</h1>
+//             <Button variant="outline-primary" className="m-1" href="https://react-bootstrap.github.io/">
+//                 Click me
+//             </Button>{' '}
+//             <hr />
+// ;<Form>
+//     <Form.Group className="mb-3" controlId="formBasicEmail">
+//         <Form.Label>Email address</Form.Label>
+//         <Form.Control type="email" placeholder="Enter email" />
+//         <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+//     </Form.Group>
 
-export default App
+//     <Form.Group className="mb-3" controlId="formBasicPassword">
+//         <Form.Label>Password</Form.Label>
+//         <Form.Control type="password" placeholder="Password" />
+//     </Form.Group>
+//     <Form.Group className="mb-3" controlId="formBasicCheckbox">
+//         <Form.Check type="checkbox" label="Check me out" />
+//     </Form.Group>
+//     <Button variant="primary" type="submit">
+//         Submit
+//     </Button>
+// </Form>
+//         </div>
+//     )
+// }
+
+// export default App
